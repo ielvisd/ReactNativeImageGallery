f91c916a5a3e068ffe021bd28476f5aa7a495e84
elvis-narrate <elvis@narrateapp.com>
2020-03-23 14:54:04 -0700

    Base image gallery example

--- App.js
+++ App.js
@@ -1,114 +1,145 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+/*This is an Example of Grid Image Gallery in React Native*/
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
 } from 'react-native';
+import FastImage from 'react-native-fast-image';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageuri: '',
+      ModalVisibleStatus: false,
+    };
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+  ShowModalFunction(visible, imageURL) {
+    //handler to handle the click on image of Grid
+    //and close button on modal
+    this.setState({
+      ModalVisibleStatus: visible,
+      imageuri: imageURL,
+    });
+  }
+
+  componentDidMount() {
+    var that = this;
+    let items = Array.apply(null, Array(120)).map((v, i) => {
+      return {id: i, src: 'https://unsplash.it/400/400?image=' + (i + 1)};
+    });
+    that.setState({
+      dataSource: items,
+    });
+  }
+
+  render() {
+    if (this.state.ModalVisibleStatus) {
+      return (
+        <Modal
+          transparent={false}
+          animationType={'fade'}
+          visible={this.state.ModalVisibleStatus}
+          onRequestClose={() => {
+            this.ShowModalFunction(!this.state.ModalVisibleStatus, '');
+          }}>
+          <View style={styles.modelStyle}>
+            <FastImage
+              style={styles.fullImageStyle}
+              source={{uri: this.state.imageuri}}
+              resizeMode={FastImage.resizeMode.contain}
+            />
+            <TouchableOpacity
+              activeOpacity={0.5}
+              style={styles.closeButtonStyle}
+              onPress={() => {
+                this.ShowModalFunction(!this.state.ModalVisibleStatus, '');
+              }}>
+              <FastImage
+                source={{
+                  uri:
+                    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/close.png',
+                }}
+                style={{width: 35, height: 35, marginTop: 16}}
+              />
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </Modal>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text
+            style={{
+              padding: 16,
+              fontSize: 20,
+              color: 'white',
+              backgroundColor: 'green',
+            }}>
+            Image Gallery
+          </Text>
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={({item}) => (
+              <View style={{flex: 1, flexDirection: 'column', margin: 1}}>
+                <TouchableOpacity
+                  key={item.id}
+                  style={{flex: 1}}
+                  onPress={() => {
+                    this.ShowModalFunction(true, item.src);
+                  }}>
+                  <FastImage
+                    style={styles.image}
+                    source={{
+                      uri: item.src,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+            //Setting the number of column
+            numColumns={3}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+      );
+    }
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    marginTop: 30,
   },
-  engine: {
+  image: {
+    height: 120,
+    width: '100%',
+  },
+  fullImageStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '98%',
+    resizeMode: 'contain',
+  },
+  modelStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  closeButtonStyle: {
+    width: 25,
+    height: 25,
+    top: 9,
+    right: 9,
     position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
-
-export default App;
